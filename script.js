eeac9574388bd4035cbf669f3af51bd0c48eae9d
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-02-08 04:28:03 +0530

    array

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-let person = {
-  name: "Priya",
-  age: 20,
-  gender: "female",
-  address: "Erode,TamilNadu",
-};
-console.log(person);
-console.log(person.age);
-//Dot notation
-person.age = 25;
-console.log(person.age);
-//bracket notation
-console.log(person["name"]);
+//array
+
+favColors = ["Black", "blue", "Green", "Pink"];
+console.log(favColors[2]);
+favColors[5] = "White";
+console.log(favColors[4]);
+console.log(favColors.length);
