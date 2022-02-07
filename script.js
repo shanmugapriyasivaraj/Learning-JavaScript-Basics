dd724953ede570d1c1908b8be37970eb09b3b898
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-02-07 17:45:57 +0530

    variables

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+let name = "priya";
+let msg = " My name is " + name + " I'm learning JavaScript";
+alert(msg);
+console.log(msg);
+
+let number1 = 40;
+let number2 = 65;
+
+let result = number1 + number2;
+console.log(result);
