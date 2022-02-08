1d97f951b3928ae59bfe7025849527174d0bca27
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-02-08 16:12:30 +0530

    arithmetic operator

--- script.js
+++ script.js
@@ -6,31 +6,57 @@
 // // console.log(favColors[4]);
 // console.log(favColors.length);
 
-function greetUser(firstname, lastname) {
-  //   let name = "Priya";
-  let msg = "Hello" + firstname + lastname + "I amm Learning JavaScript";
-  console.log(msg);
-}
+// function greetUser(firstname, lastname) {
+//   //   let name = "Priya";
+//   let msg = "Hello" + firstname + lastname + "I amm Learning JavaScript";
+//   console.log(msg);
+// }
 
-greetUser("Priya", "Sivaraj");
-greetUser("Anu", "Pallavi");
+// greetUser("Priya", "Sivaraj");
+// greetUser("Anu", "Pallavi");
 
-function add(number1, number2) {
-  console.log(number1 + number2);
-}
-add(254852, 528);
+// function add(number1, number2) {
+//   console.log(number1 + number2);
+// }
+// add(254852, 528);
 
-function sub(number1, number2) {
-  console.log(number1 - number2);
-}
-sub(254852, 528);
+// function sub(number1, number2) {
+//   console.log(number1 - number2);
+// }
+// sub(254852, 528);
 
-function mul(number1, number2) {
-  console.log(number1 * number2);
-}
-mul(254852, 528);
+// function mul(number1, number2) {
+//   console.log(number1 * number2);
+// }
+// mul(254852, 528);
 
-function div(number1, number2) {
-  console.log(number1 / number2);
-}
-div(254852, 528);
+// function div(number1, number2) {
+//   console.log(number1 / number2);
+// }
+// div(254852, 528);
+
+// Arithmetic Operators
+let number1 = 10;
+let number2 = 15;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 % number2);
+console.log(number1 ** number2);
+
+// increment
+console.log(++number1);
+console.log(number1);
+
+console.log(number1++);
+console.log(number1);
+
+// Decrement
+
+console.log(--number1);
+console.log(number1);
+
+console.log(number1--);
+console.log(number1);
