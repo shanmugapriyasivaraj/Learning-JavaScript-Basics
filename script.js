000823c53dfd8d564c9d46fa34f3295b2fcdd9cd
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-02-08 15:53:03 +0530

    function

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-//array
+// //array
 
-favColors = ["Black", "blue", "Green", "Pink"];
-console.log(favColors[2]);
-favColors[5] = "White";
-console.log(favColors[4]);
-console.log(favColors.length);
+// // favColors = ["Black", "blue", "Green", "Pink"];
+// // console.log(favColors[2]);
+// favColors[5] = "White";
+// // console.log(favColors[4]);
+// console.log(favColors.length);
+
+function greetUser(firstname, lastname) {
+  //   let name = "Priya";
+  let msg = "Hello" + firstname + lastname + "I amm Learning JavaScript";
+  console.log(msg);
+}
+
+greetUser("Priya", "Sivaraj");
+greetUser("Anu", "Pallavi");
+
+function add(number1, number2) {
+  console.log(number1 + number2);
+}
+add(254852, 528);
+
+function sub(number1, number2) {
+  console.log(number1 - number2);
+}
+sub(254852, 528);
+
+function mul(number1, number2) {
+  console.log(number1 * number2);
+}
+mul(254852, 528);
+
+function div(number1, number2) {
+  console.log(number1 / number2);
+}
+div(254852, 528);
